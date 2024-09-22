357b7cc418f1dc825a750812cfd238fb60ed76f8
adonaivillalobos <vil21022@byui.edu>
2024-09-23 00:14:48 +0200

    js

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -13,8 +13,9 @@ async function getProphetData() {
     // Convert the response to a JSON object using the .json method and store the results in a const variable named "data".
     const data = await response.json();
 
-    // Add a console.table() expression method to check the data response at this point in the console window.
-    console.table(data);
+    // Comment out the console line and call a function named "displayProphets" and include the "data.prophets" argument.
+    // console.table(data);
+    displayProphets(data.prophets);
   } catch (error) {
     console.error(`Error fetching data: ${error}`);
   }
