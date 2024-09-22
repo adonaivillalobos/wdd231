3a9742835effaacafdd800c694bc0f58bf1543d1
adonaivillalobos <vil21022@byui.edu>
2024-09-23 00:12:56 +0200

    js

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -1,31 +1,24 @@
+// Declare a const variable named "url" that contains the URL string of the JSON resource provided.
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 
+// Declare a const variable name "cards" that selects the HTML div element from the document with an id value of "cards".
 const cards = document.querySelector('#cards');
 
+// Create a async defined function named "getProphetData" to fetch data from the JSON source url using the await fetch() method.
 async function getProphetData() {
+  try {
+    // Store the response from the fetch() method in a const variable named "response".
     const response = await fetch(url);
+
+    // Convert the response to a JSON object using the .json method and store the results in a const variable named "data".
     const data = await response.json();
-    console.table(data.prophets); // temporary testing of data response
+
+    // Add a console.table() expression method to check the data response at this point in the console window.
+    console.table(data);
+  } catch (error) {
+    console.error(`Error fetching data: ${error}`);
+  }
 }
+
+// Call the function getProphetData() in the main line of your .js code to test the fetch and response.
 getProphetData();
-
-async function getProphetData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    //console.table(data.prophets);
-    displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
-  }
-  
-  getProphetData();
-
-  const displayProphets = (prophets) => {
-    // card build code goes here
-
-    const displayProphets = (prophets) => {
-        prophets.forEach((prophet) => {
-      
-        });
-      }
-  }
-
-  
