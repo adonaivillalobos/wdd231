d42c841a7dcb7c670fb2a1e3fe99725cb1ad5c8c
adonaivillalobos <vil21022@byui.edu>
2024-09-23 00:08:23 +0200

    js

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -8,3 +8,24 @@ async function getProphetData() {
     console.table(data.prophets); // temporary testing of data response
 }
 getProphetData();
+
+async function getProphetData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
+  }
+  
+  getProphetData();
+
+  const displayProphets = (prophets) => {
+    // card build code goes here
+
+    const displayProphets = (prophets) => {
+        prophets.forEach((prophet) => {
+      
+        });
+      }
+  }
+
+  
